ab6546c39a3eb98c665839c561dc9b4aaa6a89ca
pasqualeconte <conte.pas@me.com>
2020-12-14 21:49:28 +0100

    Add emit movement direction in GameVehicle.

--- src/components/GameVehicle.vue
+++ src/components/GameVehicle.vue
@@ -1,22 +1,18 @@
 <template>
-    <div :style="style" :class="$style.vehicle">
-        <div>{{}}</div>
-        <div>{{}}</div>
-    </div>
+    <div
+        :style="style"
+        :class="$style.vehicle"
+        ref="gameElement"
+        @click="move($event)"
+    />
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, PropType } from 'vue'
+import { defineComponent, computed, PropType, ref, Ref } from 'vue'
 
 export default defineComponent({
     name: 'GameVehicle',
     props: {
-        // elementType: {
-        //     type: String,
-        //     required: true,
-        //     validation: (elementType: string) =>
-        //         ['redCar', 'vehicle', 'obstacle'].includes(elementType),
-        // },
         elementId: {
             type: String,
             required: true,
@@ -26,22 +22,19 @@ export default defineComponent({
             required: true,
         },
     },
-    setup(props) {
+    setup(props, { emit }) {
         if (!props.positionRaw?.length || props.positionRaw?.length < 1)
             throw `positionRaw can't be empty`
-            
-        const direction = computed(() => {
+
+        const orientation = computed(() => {
             const { positionRaw } = props
-            console.log('POSITION')
-            console.log(props.elementId)
-            console.log(positionRaw)
-            return (positionRaw[0] + 1) === positionRaw[1]
+            return positionRaw[0] + 1 === positionRaw[1]
                 ? 'orizzontal'
                 : 'vertical'
         })
         const measures = computed(() => {
             const { positionRaw } = props
-            const vertical = direction.value === 'vertical'
+            const vertical = orientation.value === 'vertical'
             return {
                 width: vertical ? 100 : 100 * positionRaw.length,
                 height: vertical ? 100 * positionRaw.length : 100,
@@ -49,9 +42,9 @@ export default defineComponent({
         })
         const position = computed(() => {
             const { positionRaw } = props
-            const vertical = direction.value === 'vertical'
+            const vertical = orientation.value === 'vertical'
             return {
-                top: Math.trunc(positionRaw[0] / 6) * 100, //vertical ? positionRaw.length * 100 : (positionRaw[0] % 6) * 100,
+                top: Math.trunc(positionRaw[0] / 6) * 100,
                 left: (positionRaw[0] % 6) * 100,
             }
         })
@@ -60,11 +53,26 @@ export default defineComponent({
             const { top, left } = position.value
             return `width: ${width}px; height: ${height}px; top: ${top}px; left: ${left}px`
         })
+
+        const gameElement: Ref<HTMLElement | null> = ref(null)
+        const move = (event: MouseEvent) => {
+            let direction = null
+            const element = gameElement.value?.getBoundingClientRect()
+            if (orientation.value === 'vertical') {
+                const y = element ? event.clientY - element.top : null
+                direction = y && y < measures.value.height / 2 ? 'up' : 'down'
+            } else {
+                const x = element ? event.clientX - element.left : null
+                direction = x && x < measures.value.width / 2 ? 'left' : 'right'
+            }
+            emit('move', direction)
+        }
         return {
-            direction,
             measures,
             position,
             style,
+            move,
+            gameElement,
         }
     },
 })
